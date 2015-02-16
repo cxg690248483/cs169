152bb393c5b42bd8697d86a99966a650b664645f
saasbook <saasbook@saasbook.(none)>
2015-02-16 19:14:34 -0400

    hw2 init

---
+++ rottenpotatoes/rottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = 'bc1a885071451ee3aa0b94a84e26eff0c1889d9d2aad55c8520aadab3963521d5166d05abf7e6e4fbe3bd0a85906141f12a7c1119544522d94f81d7cd781c9a2'
