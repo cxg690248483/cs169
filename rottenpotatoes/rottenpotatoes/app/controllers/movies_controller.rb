8b170b11b6f5d79b5700cb6547231bc78263d7e2
saasbook <saasbook@saasbook.(none)>
2015-02-17 07:08:04 -0400

    part2 3 fininshed

--- rottenpotatoes/rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/rottenpotatoes/app/controllers/movies_controller.rb
@@ -7,13 +7,54 @@ class MoviesController < ApplicationController
   end
 
   def index
-    if(params[:sort] == 'titleheader')
-      @movies = Movie.order(:title)
-    elsif(params[:sort] == 'dateheader')
-      @movies = Movie.order(:release_date)
-    else
-      @movies = Movie.all
+    #part3  remember
+    if(params[:sort] == nil && params[:ratings] == nil)
+      if(session[:sort] != nil || session[:ratings] != nil)
+        redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+      end
     end
+    
+    #part1  sorting
+    @sort = params[:sort]
+    if(@sort == 'titleheader')
+      @sort = :title
+    elsif(@sort == 'dateheader')
+      @sort = :release_date
+    end
+    session[:sort] = @sort
+
+    
+    #part2  rating checkbox
+    @all_ratings = Movie.all_ratings.keys
+    @ratings = params[:ratings]
+    if(@ratings != nil)
+      ratings = @ratings.keys
+      session[:ratings] = @ratings
+    #if no current input ratings, but input sort, use last time input ratings
+    else
+      if(params['commit'] == nil && params['sort'] == nil)
+        ratings = Movie.all_ratings.keys
+        session[:ratings] = Movie.all_ratings
+      else
+        ratings = session[:ratings].keys
+      end
+    #if no current input ratings and input sort, ratings filter = all_ratings
+#else
+#ratings = Movie.all_ratings
+#session[:ratings] = Movie.all_ratings
+    end
+
+    if(@sort != nil) 
+      @movies = Movie.order(@sort).find_all_by_rating(ratings)
+    else
+      @movies = Movie.find_all_by_rating(ratings)
+    end
+    
+    #indicate checked boxes
+    @checked  = ratings
+  
+#@movies = Movie.find_all_by_rating(ratings)
+
   end
 
   def new
--- rottenpotatoes/rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/rottenpotatoes/app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+  def self.all_ratings
+    {'G'=>'1', 'PG'=>'1', 'PG-13'=>'1', 'R'=>'1'}
+  end
 end
